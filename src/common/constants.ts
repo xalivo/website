45d8417a1760c7543c5c89a8c6073c53cd1d74bd
xalivo <87896066+xalivo@users.noreply.github.com>
2026-05-24 20:44:26 +0200

    added constants for flower types & colors, added URL for flower images

---
+++ src/common/constants.ts
@@ -0,0 +1,3 @@
+// tools: flower
+const flowerTypes = ["rose", "tulip", "orchid"];
+const flowerColors = ["red", "blue", "yellow", "pink"];
--- src/common/deploy-config.ts
+++ src/common/deploy-config.ts
@@ -1,2 +1,5 @@
 export const API_URL = "";
 export const STATIC_API_URL = "https://static.xalivo.com";
+
+// tools: flower
+export const FLOWER_IMAGE_BASE_URL = STATIC_API_URL + "/images/flower/";
